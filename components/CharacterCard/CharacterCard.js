cccf013a728598a79d2b711aa6397892c0f2bdcf
Gunnar <gunnar.montag@gmx.de>
2024-06-19 16:49:11 +0200

    add character cards

--- components/CharacterCard/CharacterCard.js
+++ components/CharacterCard/CharacterCard.js
@@ -1,1 +1,26 @@
-export function CharacterCard() {}
+export function CharacterCard(data) {
+  const card = document.createElement("li");
+  card.classList.add("card");
+  card.innerHTML = `
+              <div class="card__image-container">
+            <img
+              class="card__image"
+              src="${data.image}"
+              alt="${data.name}"
+            />
+            <div class="card__image-gradient"></div>
+          </div>
+          <div class="card__content">
+            <h2 class="card__title">${data.name}</h2>
+            <dl class="card__info">
+              <dt class="card__info-title">Status</dt>
+              <dd class="card__info-description">${data.status}</dd>
+              <dt class="card__info-title">Type</dt>
+              <dd class="card__info-description">${data.species}</dd>
+              <dt class="card__info-title">Occurrences</dt>
+              <dd class="card__info-description">${data.gender}</dd>
+            </dl>
+          </div>
+    `;
+  return card;
+}
--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+import { CharacterCard } from "./components/CharacterCard/CharacterCard.js";
+
 const cardContainer = document.querySelector('[data-js="card-container"]');
 const searchBarContainer = document.querySelector(
   '[data-js="search-bar-container"]'
@@ -12,3 +14,32 @@ const pagination = document.querySelector('[data-js="pagination"]');
 const maxPage = 1;
 const page = 1;
 const searchQuery = "";
+
+const urlCharacters = "https://rickandmortyapi.com/api/character";
+const urlLocation = "https://rickandmortyapi.com/api/location";
+const urlEpisodes = "https://rickandmortyapi.com/api/episodes";
+
+async function fetchCharacters() {
+  const response = await fetch(urlCharacters);
+  const data = await response.json();
+  const characterData = data.results;
+  cardContainer.innerHTML = "";
+  characterData
+    .map(CharacterCard)
+    /* .map((character) => {
+      CharacterCard(character);
+    }) */
+    .forEach((card) => {
+      cardContainer.append(card);
+    });
+
+  /* return characterData; */
+
+  /*   characterData.results.forEach((character) => {
+    const cardElement = CharacterCard(character);
+    cardContainer.innerHTML = "";
+    cardContainer.append(cardElement);
+  }); */
+}
+
+fetchCharacters();
